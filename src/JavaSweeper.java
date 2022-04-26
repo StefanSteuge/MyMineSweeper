ed983a0c6fef4815b48518dd1481ce361c1d2951
Stepan Dulka <fotosteuge@gmail.com>
2022-04-26 23:13:56 +0300

    Continue

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -2,11 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 
 import sweeper.Box;
+import sweeper.Coord;
+import sweeper.Game;
+import sweeper.Ranges;
 
 public class JavaSweeper extends JFrame {
+    private Game game;
     private JPanel panel;
-    private final int COLS = 15;
-    private final int ROWS = 1;
+    private final int COLS = 9;
+    private final int ROWS = 9;
+    private final int BOMBS = 10;
     private final int IMAGE_SIZE = 50;
 
     public static void main(String[] args) {
@@ -15,23 +20,28 @@ public class JavaSweeper extends JFrame {
     }
 
     private JavaSweeper() {
+        game = new Game(COLS, ROWS,BOMBS);
+        game.start();
         setImages();
         initPanel();
         initFrame();
     }
+
 
     private void initPanel() {
         panel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                for (Box box : Box.values())
-                    g.drawImage((Image) box.image, box.ordinal() * IMAGE_SIZE, 0, this);
+                for (Coord coord : Ranges.getAllCoords())
+                    g.drawImage((Image) game.getBox(coord).image,
+                            coord.x * IMAGE_SIZE, coord.y * IMAGE_SIZE, this);
 
             }
         };
         panel.setPreferredSize(new Dimension(
-                COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
+                Ranges.getSize().x * IMAGE_SIZE,
+                Ranges.getSize().y * IMAGE_SIZE));
         add(panel);
     }
 
@@ -42,6 +52,7 @@ public class JavaSweeper extends JFrame {
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
+        setIconImage(getImage("icon"));
     }
 
     private void setImages() {
